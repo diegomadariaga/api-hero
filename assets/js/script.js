fbea4b98523d028e0aa19c0296391326d342fb72
diego <diegonicolasmadariaga@gmail.com>
2021-06-18 21:59:18 -0400

    asd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
 
-    $('#contenido').hide();
+    $('#contenido').hide();//se esconde la seccion de contenido
     
     $('#btn_buscar').click(function (e) { 
         e.preventDefault();
-
+        //se lee la variable id para buscar
         let id = $("#txt_idsp").val();
         
-        if (validar(id)) {
+        if (validar(id)) {//se valida que sea numero
             //$('#contenido').hide(300);
-            consulta_api(id);
-            $('#contenido').show(); 
+            consulta_api(id);//se ejecuta la consulta
+            $('#contenido').show(); // se muestra el contenido
         }
     });
 
@@ -43,10 +43,10 @@ $(document).ready(function () {
                 dataPoints: stats //se asigna el arreglo
             }]
         });
-        chart.render();
+        chart.render();// se renderiza el canvas
     }
     
-    function validar(input){            
+    function validar(input){   // se valida que sea un numero y no este vacio         
         if(input == '' ){            
             $("#errorid").text("primero ingrese un numero");
             return false;
@@ -60,24 +60,25 @@ $(document).ready(function () {
     }
 
     function consulta_api(id) {
-        
+        //se ejecuta consulta a la api por ajax
         $.ajax({
             type: "GET",
             url:        "https://www.superheroapi.com/api.php/10160906972594027/"+id ,
             success:    function(data) {
-                console.log(data);
                 
+                //se verifica que no tenga error
                 if (data.response=="error") {
                     alert(data.error);
                 } else {
                     let nombre      = data.name;
                     let conexiones  = data.connections['group-affiliation'];
+                    //se leen variables y crea el parrafo
 
                     let parrafo1    = `
                         <h5>Nombre: ${nombre}</h5>
                         <p>conexiones: ${conexiones}</p>
                     `;
-                    
+                    //se asigna el parrago al html
                     $("#div_hero1").html(parrafo1);
 
                     let publicado           = data.biography.publisher;
@@ -87,11 +88,12 @@ $(document).ready(function () {
                     let peso                = data.appearance.weight['0']+ " - "+data.appearance.weight['1'];
                     let alianzas            = "";
                     
+                    //se recorre el objeto para obtener alianzas
                     for (const property in data.biography.aliases) {
                         alianzas = alianzas+` ${data.biography.aliases[property]}`;
                     }
                     
-
+                    //se crea el sagundo parrafo con las variables
                     let parrafo2    = `
                     <small>
                         <p>publicado por: ${publicado}</p>
@@ -103,12 +105,12 @@ $(document).ready(function () {
                     </small>
                     `;
 
-                    $("#div_hero2").html(parrafo2);
+                    $("#div_hero2").html(parrafo2);//se asigna el parrrafo
                     
-                    $("#imagen").attr("src",data.image.url);
+                    $("#imagen").attr("src",data.image.url);//se asigna la imagen
 
 
-                    mostrar_grafico(data);
+                    mostrar_grafico(data);//se muestra el grafico y se le pasa el objeto
                 }
                 
             },
